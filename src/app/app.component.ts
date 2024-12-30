f6ef5b13878fcde0574feed09432cd9d9b6fe5b9
Jaf-b <jafredtshikaya@gmail.com>
2024-12-30 12:17:38 +0100

    version initiale

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,15 @@
-import { Component } from '@angular/core';
+import { serverRoutes } from './app.routes.server';
+import { Component, inject, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ApiService } from './core/services/api.service';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'ngDuka';
+  private api = inject(ApiService);
 }
